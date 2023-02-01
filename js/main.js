a082f05f1800384c01f172d99e52b55f00181821
workingUnd <122107760+workingUnd@users.noreply.github.com>
2023-02-01 12:23:18 +0100

    New
    Создание и адаптация трех блоков

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-
-
+   $(document).ready(function () {
+                $(".owl-carousel").owlCarousel({
+                    margin: 35,
+                     responsive:{
+                         0: {
+                             items: 1
+                         },
+                         700:{
+                             items: 2
+                         },
+                         1000:{
+                             items: 3
+                         }
+                     }
+                });
+            });
