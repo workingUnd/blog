152f7e4c1a37958097a91427a658993a0869e432
workingUnd <122107760+workingUnd@users.noreply.github.com>
2023-02-06 19:38:48 +0100

    Refresh
    Refresh faile

--- js/main.js
+++ js/main.js
@@ -13,4 +13,23 @@
                          }
                      }
                 });
+                $(".slide-two").owlCarousel({
+                    margin: 35,
+                     responsive:{
+                         0: {
+                             items: 1
+                         },
+                         700:{
+                             items: 2
+                         },
+                         1000:{
+                             items: 3
+                         }
+                     }
+                });
+                 $(".slide-tree").owlCarousel({
+                    items:1,
+                    center: true
+                });
             });
+            
