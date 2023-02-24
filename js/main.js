e5fa9109c4ec1aeae1a1f31060ef8bff26e205c6
workingUnd <122107760+workingUnd@users.noreply.github.com>
2023-02-24 12:37:40 +0100

    update
    update tree page

--- js/main.js
+++ js/main.js
@@ -1,35 +1,42 @@
-   $(document).ready(function () {
-                $(".owl-carousel").owlCarousel({
-                    margin: 35,
-                     responsive:{
-                         0: {
-                             items: 1
-                         },
-                         700:{
-                             items: 2
-                         },
-                         1000:{
-                             items: 3
-                         }
-                     }
-                });
-                $(".slide-two").owlCarousel({
-                    margin: 35,
-                     responsive:{
-                         0: {
-                             items: 1
-                         },
-                         700:{
-                             items: 2
-                         },
-                         1000:{
-                             items: 3
-                         }
-                     }
-                });
-                 $(".slide-tree").owlCarousel({
-                    items:1,
-                    center: true 
-                });
-            });
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        margin: 35,
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    $(".slide-two").owlCarousel({
+        margin: 35,
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    $(".slide-4").owlCarousel({
+        margin: 35,
+        responsive: {
+            0: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
   
