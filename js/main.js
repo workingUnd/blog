5eeee93fa64d15776e13690335f0745c7e4a6b86
workingUnd <122107760+workingUnd@users.noreply.github.com>
2023-03-06 18:47:36 +0100

    Update
    update page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
+    $(".slide-one").owlCarousel({
         margin: 35,
         responsive: {
             0: {
@@ -33,10 +33,96 @@ $(document).ready(function () {
             0: {
                 items: 1
             },
+            700: {
+                items: 2
+            },
             1000: {
                 items: 3
             }
         }
     });
+    $(".sliderfift").owlCarousel({
+        margin: 35,
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    $(".slidersext").owlCarousel({
+        margin: 35,
+        dotsEach: true,
+        slideBy: 1,
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    $(".slide-fo").owlCarousel({
+        margin: 35,
+        dotsEach: true,
+        slideBy: 1,
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    $(".slide-tree").owlCarousel({
+        items: 1,
+        center: true,
+        dotsEach: true
+    });
+    $(".slide-five").owlCarousel({
+        responsive: {
+            0: {
+                items: 2
+            },
+            700: {
+                items: 5
+            },
+            1000: {
+                items: 8
+            }
+        },
+        margin: 30,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true
+    });
+    $(".slide-sex").owlCarousel({
+         responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        },
+        margin: 35
+    });
 });
-  
+
